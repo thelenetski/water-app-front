ad457ded4651ad27c78448b3650bc59512493011
Kateryna_Yershova <yershova.kate@icloud.com>
2024-11-24 17:19:04 +0200

    add changes

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,31 +1,14 @@
-import React from 'react';
 import styles from './AdvantagesSection.module.css';
-
-const SimpleList = ({ items }) => {
-    return (
-        <ul className={styles.advantagesList}>
-            {items.map((item, index) => (
-                <li
-                    key={index}
-                    className={`${styles.advantagesItem} ${
-                        item === "Habit drive" ? styles.withDot : ""
-                    }`}
-                >
-                    {item}
-                </li>
-            ))}
-        </ul>
-    );
-};
+import AdvantagesList from '../AdvantagesList/AdvantagesList';
 
 const AdvantagesSection = () => {
-    const advantages = ["Habit drive", "View statistics", "Personal rate setting"];
+    const advantages = ['Habit drive', 'View statistics', 'Personal rate setting'];
 
     return (
         <section className={styles.advantagesSection}>
             <div className={styles.advantagesContent}>
                 <p className={styles.advantagesCaption}>Our happy customers</p>
-                <SimpleList items={advantages} />
+                <AdvantagesList items={advantages} />
             </div>
         </section>
     );
