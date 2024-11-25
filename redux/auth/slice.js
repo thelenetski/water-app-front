a4478510e50831ce713fdb2577494ed67dc5104d
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-11-25 13:22:18 +0200

    add redux

---
+++ redux/auth/slice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { signUp, signIn, logOut, refreshUser } from "./operations";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: {
+      email: null,
+      password: null,
+    },
+    token: null,
+    isSignedIn: false,
+    isRefreshing: false,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isSignedIn = true;
+      })
+      .addCase(signIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isSignedIn = true;
+      })
+      .addCase(logOut.fulfilled, (state) => {
+        state.user = { email: null };
+        state.token = null;
+        state.isSignedIn = false;
+      })
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isSignedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state) => {
+        state.isRefreshing = false;
+      });
+  },
+});
+
+export const authReducer = authSlice.reducer;
