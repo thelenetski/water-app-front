999932e2a65a17b008f377b483955cb9fbd7f8a2
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-11-25 20:44:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import SignInForm from "./components/SignInForm/SignInForm";
 
 function App() {
-  return (
-    <div>
-      <h1>AquaTrack</h1>
-      <SignInForm />
-    </div>
-  );
+  return <h1>AquaTrack</h1>;
 }
 
 export default App;
