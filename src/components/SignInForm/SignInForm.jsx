59a1cec21b957dd7b5b87822b55229dd95bad62a
Nika <nikapanchenko94@gmail.com>
2024-11-26 09:52:26 +0200

    added validation schema and styles

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,5 +1,11 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import css from "./SignInForm.module.css";
+import * as Yup from "yup";
+
+const UserValidationSchema = Yup.object().shape({
+  userEmail: Yup.string().email("Must be a valid email!").required("Required"),
+  userPassword: Yup.string().required("No password provided"),
+});
 
 const INITIAL_VALUES = { userEmail: "", userPassword: "" };
 
@@ -9,31 +15,51 @@ const SignInForm = () => {
     actions.resetForm();
   };
   return (
-    <div>
-      <h2 className={css.title}>Sign In</h2>
-      <Formik initialValues={INITIAL_VALUES} onSubmit={handleSubmit}>
+    <div className={css.formContainer}>
+      <Formik
+        initialValues={INITIAL_VALUES}
+        onSubmit={handleSubmit}
+        validationSchema={UserValidationSchema}
+      >
         <Form className={css.form}>
+          <h2 className={css.title}>Sign In</h2>
           <label>
-            <span>Email</span>
+            <span className={css.inputLabel}>Email</span>
             <Field
               className={css.field}
               type="email"
               name="userEmail"
               placeholder="Enter your email"
+              aria-label="Email"
+            />
+            <ErrorMessage
+              className={css.error}
+              name="userEmail"
+              component="span"
             />
           </label>
           <label>
-            <span>Password</span>
+            <span className={css.inputLabel}>Password</span>
             <Field
               className={css.field}
               type="password"
               name="userPassword"
               placeholder="Enter your password"
+              aria-label="Password"
+            />
+            <ErrorMessage
+              className={css.error}
+              name="userPassword"
+              component="span"
             />
           </label>
           <button className={css.button} type="submit">
             Sign in
           </button>
+          <p className={css.signuptext}>
+            Don’t have an account?{" "}
+            <span className={css.signuplink}>Sign Up</span>
+          </p>
         </Form>
       </Formik>
     </div>
