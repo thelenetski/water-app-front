c72d90c13a7c51af029f34385c4f1a5182006368
Sofiia Hoppova <sofiahoppova@gmail.com>
2024-11-27 14:51:40 +0200

    add updated sprite, favicon and validation to settings form

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -1,23 +1,36 @@
+import { useEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { BsExclamationLg } from "react-icons/bs";
 import { FiUpload } from "react-icons/fi";
+import avatar from "../../img/default-avatar.webp";
 
 import css from "./UserSettingsForm.module.css";
 
 const validationParams = Yup.object().shape({
   name: Yup.string().min(3, "Too Short!").max(50, "Too Long!"),
-  gender: Yup.string(),
+  gender: Yup.string().oneOf(["woman", "man"]),
   email: Yup.string().email("Enter a valid email!").required("Required"),
   photo: Yup.object().shape({
     name: Yup.string().required(),
   }),
-  weight: Yup.number().positive("Enter a valid value"),
-  hoursOfSport: Yup.number().positive("Enter a valid value"),
-  dailyNorma: Yup.number().positive("Enter a valid value"),
+  weight: Yup.number()
+    .max(500, "Enter a valid value")
+    .positive("Enter a valid value"),
+  hoursOfSport: Yup.number()
+    .max(24, "Enter a valid value")
+    .positive("Enter a valid value"),
+  dailyNorma: Yup.number()
+    .max(15, "Enter a valid value")
+    .positive("Enter a valid value"),
 });
 
 const UserSettingsForm = () => {
+  const [preview, setPreview] = useState(null);
+  const [weight, setWeight] = useState(0);
+  const [hoursOfSport, setHoursOfSport] = useState(0);
+  const [recommendedDailyNorma, setRecommendedDailyNorma] = useState(null);
+
   const initialValues = {
     name: "",
     gender: "",
@@ -26,6 +39,24 @@ const UserSettingsForm = () => {
     weight: "",
     hoursOfSport: "",
     dailyNorma: "",
+  };
+
+  useEffect(() => {
+    let norma;
+    if (female === "woman") {
+      norma = weight * 0.03 + hoursOfSport * 0.4;
+      setRecommendedDailyNorma(norma.toFixed(1));
+    } else {
+      norma = weight * 0.04 + hoursOfSport * 0.6;
+      setRecommendedDailyNorma(norma.toFixed(1));
+    }
+  }, [weight, hoursOfSport]);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setPreview(URL.createObjectURL(file)); // Створення URL для файлу
+    }
   };
 
   const handleSubmit = (values, actions) => {
@@ -40,6 +71,11 @@ const UserSettingsForm = () => {
     >
       <Form className={css.formWrapper}>
         <div className={css.fileWrapper}>
+          {preview ? (
+            <img className={css.avatarImg} src={preview} alt="User's avatar" />
+          ) : (
+            <img className={css.avatarImg} src={avatar} alt="User's avatar" />
+          )}
           <label>
             <div className={css.iconWrapper}>
               <FiUpload className={css.icon} />
@@ -48,6 +84,7 @@ const UserSettingsForm = () => {
                 name="photo"
                 placeholder="Upload a photo"
                 hidden
+                onChange={handleFileChange}
               />
               <span>Upload a photo</span>
             </div>
@@ -62,14 +99,14 @@ const UserSettingsForm = () => {
                 <Field
                   type="radio"
                   name="gender"
-                  value="Woman"
+                  value="woman"
                   checked={true}
                 />
                 <span className={css.checkmark}></span>
                 <span>Woman</span>
               </label>
               <label className={css.radioWrapper}>
-                <Field type="radio" name="gender" value="Man" />
+                <Field type="radio" name="gender" value="man" />
                 <span className={css.checkmark}></span>
                 <span>Man</span>
               </label>
@@ -126,7 +163,15 @@ const UserSettingsForm = () => {
             <div className={css.blockWrapper}>
               <label className={css.inputWrapper}>
                 <span>Your weight in kilograms:</span>
-                <Field type="text" name="weight" placeholder="0" />
+                <Field
+                  type="text"
+                  name="weight"
+                  placeholder="0"
+                  value={weight}
+                  onChange={(e) => {
+                    setWeight(e.target.value);
+                  }}
+                />
                 <ErrorMessage
                   className={css.errorMessage}
                   name="weight"
@@ -135,7 +180,15 @@ const UserSettingsForm = () => {
               </label>
               <label className={css.inputWrapper}>
                 <span>The time of active participation in sports:</span>
-                <Field type="text" name="hoursOfSport" placeholder="0" />
+                <Field
+                  type="text"
+                  name="hoursOfSport"
+                  placeholder="0"
+                  value={hoursOfSport}
+                  onChange={(e) => {
+                    setHoursOfSport(e.target.value);
+                  }}
+                />
                 <ErrorMessage
                   className={css.errorMessage}
                   name="hoursOfSport"
@@ -147,7 +200,9 @@ const UserSettingsForm = () => {
             <div className={css.blockWrapper}>
               <p>
                 The required amount of water in liters per day: <br />
-                <span className={css.accentText}>1.8L</span>
+                <span className={css.accentText}>
+                  {recommendedDailyNorma ? `${recommendedDailyNorma}L` : "1.8L"}
+                </span>
               </p>
               <label className={css.inputWrapper}>
                 <span className={css.inputLabel}>
