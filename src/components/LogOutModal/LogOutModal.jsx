aecb072dcca027488043055a71539c0f0a70908a
Sofiia Fesenko <sofia.fesen@gmail.com>
2024-11-28 21:25:48 +0200

    logout modal

---
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux'
+import css from './LogOutModal.module.css'
+import { logOut } from '../../redux/auth/operations'
+import { closeModal } from '../../redux/modal/slice'
+
+export const LogOutModal = () => {
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch(logOut())
+        dispatch(closeModal())
+    }
+
+    return (
+        <div className={css.logoutModal}>
+            <h2 className={css.title}>Log out</h2>
+            <p className={css.text}>Do you really want to leave?</p>
+            <button type="button" className={`${css.btn} ${css.logoutBtn}`} onClick={handleLogout}>Log out</button>
+            <button type="button" className={`${css.btn} ${css.cancelBtn}`} onClick={() => dispatch(closeModal())}>Cancel</button>
+        </div>
+    )
+}
