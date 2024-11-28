c61514049dd25e7a2d171a22ef4f9cb1722fe83d
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-11-28 20:08:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import { useState } from "react";
+import { lazy } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import { Layout } from "./components/Layout.jsx";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+// const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage.jsx"));
+// const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage.jsx"));
+// const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
 
 function App() {
-  //<h1>AquaTrack</h1>;
-  return <h1>AquaTrack</h1>;
+  return (
+    <div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route
+            path="/signup"
+            element={
+              <RestrictedRoute
+                component={<SignUpPage />}
+                redirectTo="/tracker"
+              />
+            }
+          />
+          <Route
+            path="/signin"
+            element={
+              <RestrictedRoute
+                component={<SignInPage />}
+                redirectTo="/tracker"
+              />
+            }
+          />
+          <Route
+            path="/tracker"
+            element={
+              <PrivateRoute component={<TrackerPage />} redirectTo="/signin" />
+            }
+          /> */}
+        </Routes>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
