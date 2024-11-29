8468a5799f021a48d42c3ac580f4ffb33444360c
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-11-29 21:13:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,36 @@ import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import { Layout } from "./components/Layout.jsx";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
 const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage.jsx"));
 const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage.jsx"));
 // const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
 
 function App() {
   return (
-    <div>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/signup"
-            element={
-              <RestrictedRoute
-                component={<SignUpPage />}
-                redirectTo="/tracker"
-              />
-            }
-          />
-          <Route
-            path="/signin"
-            element={
-              <RestrictedRoute
-                component={<SignInPage />}
-                redirectTo="/tracker"
-              />
-            }
-          />
-          {/* <Route
-            path="/tracker"
-            element={
-              <PrivateRoute component={<TrackerPage />} redirectTo="/signin" />
-            }
-          /> */}
-        </Routes>
-      </Layout>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/signup"
+          element={
+            <RestrictedRoute component={<SignUpPage />} redirectTo="/tracker" />
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <RestrictedRoute component={<SignInPage />} redirectTo="/tracker" />
+          }
+        />
+        <Route
+          path="/tracker"
+          element={
+            <PrivateRoute component={<TrackerPage />} redirectTo="/signin" />
+          }
+        />
+      </Routes>
+    </Layout>
   );
 }
 
