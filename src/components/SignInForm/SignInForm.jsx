8bb2f747e69055ade49578acdbf962ded7d9d20b
Nika <nikapanchenko94@gmail.com>
2024-12-02 07:10:58 +0200

    added all styles

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -34,9 +34,11 @@ const SignInForm = () => {
       .then(() => {
         toast.success("Login successful!");
         actions.resetForm();
+        actions.setSubmitting(false);
       })
       .catch((error) => {
         toast.error("Login failed: " + error.message);
+        actions.setSubmitting(false);
       });
   };
 
@@ -91,7 +93,7 @@ const SignInForm = () => {
                 >
                   <svg className={css.icon}>
                     <use
-                      xlinkHref={`/icons/sprite.svg#icon-${
+                      xlinkHref={`/sprite.svg#icon-${
                         showPassword ? "eye" : "eye-off"
                       }`}
                     />
