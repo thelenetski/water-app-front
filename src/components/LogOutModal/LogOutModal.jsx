d01965c28215a1747deaf313360a096d32254c04
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-12-02 09:27:00 +0200

    Update LogOutModal.jsx

--- src/components/LogOutModal/LogOutModal.jsx
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -1,24 +1,43 @@
-import { useDispatch } from 'react-redux'
-import css from './LogOutModal.module.css'
-import { logOut } from '../../redux/auth/operations'
-import { closeModal } from '../../redux/modal/slice'
+import { useDispatch } from "react-redux";
+import css from "./LogOutModal.module.css";
+import { logOut } from "../../redux/auth/operations";
+import { closeModal } from "../../redux/modal/slice";
+import toast from "react-hot-toast";
 
 const LogOutModal = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const handleLogout = () => {
-        dispatch(logOut())
-        dispatch(closeModal())
-    }
+  const handleLogout = () => {
+    dispatch(logOut())
+      .unwrap()
+      .then(() => {
+        dispatch(closeModal());
+      })
+      .catch((error) => {
+        toast.error("Logout failed: " + error.message);
+      });
+  };
 
-    return (
-        <div className={css.logoutModal}>
-            <h2 className={css.title}>Log out</h2>
-            <p className={css.text}>Do you really want to leave?</p>
-            <button type="button" className={`${css.btn} ${css.logoutBtn}`} onClick={handleLogout}>Log out</button>
-            <button type="button" className={`${css.btn} ${css.cancelBtn}`} onClick={() => dispatch(closeModal())}>Cancel</button>
-        </div>
-    )
-}
+  return (
+    <div className={css.logoutModal}>
+      <h2 className={css.title}>Log out</h2>
+      <p className={css.text}>Do you really want to leave?</p>
+      <button
+        type="button"
+        className={`${css.btn} ${css.logoutBtn}`}
+        onClick={handleLogout}
+      >
+        Log out
+      </button>
+      <button
+        type="button"
+        className={`${css.btn} ${css.cancelBtn}`}
+        onClick={() => dispatch(closeModal())}
+      >
+        Cancel
+      </button>
+    </div>
+  );
+};
 
 export default LogOutModal;
