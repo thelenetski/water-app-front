72b4239165b8f4313d7809b68d9c7ad0c775be5b
Ilya Svintsov <xeri237@gmail.com>
2024-12-02 14:21:24 +0200

    add arrows icon at calendar pagination

--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -2,7 +2,17 @@ import css from './CalendarPagination.module.css'
 
 const CalendarPagination = ({year, monthName, nextMonth, prevMonth}) => {
     return <div className={css.container}>
-    <button type="button" onClick={prevMonth}>prev</button><span className={css.text}>{`${monthName}, ${year}`}</span><button type="button" onClick={nextMonth}>next</button>
+        <button type="button" onClick={prevMonth} className={css.button}>
+            <svg className={`${css.icon} ${css.iconPrev}`}>
+                <use href="/sprite.svg#icon-chevron-down"></use>
+            </svg>
+        </button>
+        <span className={css.text}>{`${monthName}, ${year}`}</span>
+        <button type="button" onClick={nextMonth} className={css.button}>
+            <svg className={`${css.icon} ${css.iconNext}`}>
+                <use href="/sprite.svg#icon-chevron-down"></use>
+            </svg>
+        </button>
     </div>
 }
 
