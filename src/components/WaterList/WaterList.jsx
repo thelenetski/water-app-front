f92601a5defb8bbe55f1100ed1ef87df6daaaa7a
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-12-03 14:56:02 +0200

    fix some bugs with loader in app.jsx and add loading text in waterlist

--- src/components/WaterList/WaterList.jsx
+++ src/components/WaterList/WaterList.jsx
@@ -1,17 +1,20 @@
 import { useSelector } from "react-redux";
-import { selectWatersDaily } from "../../redux/waters/selectors";
+import { selectLoading, selectWatersDaily } from "../../redux/waters/selectors";
 import { WaterItem } from "../WaterItem/WaterItem";
 import css from "./WaterList.module.css";
 
 const WaterList = () => {
   const waterItems = useSelector(selectWatersDaily);
+  const loading = useSelector(selectLoading);
 
   return (
     <>
       {!waterItems?.length ? (
-        <div
-          className={css.noWater}
-        >{`You haven\u2019t added the water yet.`}</div>
+        <div className={css.noWater}>
+          {!loading ? `You haven\u2019t added the water yet.` : "Loading..."}
+        </div>
+      ) : loading ? (
+        <div className={css.noWater}>{`Loading...`}</div>
       ) : (
         <ul className={css.wrapper}>
           {waterItems.map((item) => (
