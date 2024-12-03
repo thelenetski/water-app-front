2c7a7ebab3504334bc301a4d6e4496f0bb8c520a
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-12-04 00:19:36 +0200

    fix css welcomePage, HomePage, SignupPage

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,31 +1,35 @@
-import { useNavigate } from 'react-router-dom';
-import Logo from '../Logo/Logo';
-import styles from './WelcomeSection.module.css';
+import { useNavigate } from "react-router-dom";
+import Logo from "../Logo/Logo";
+import styles from "./WelcomeSection.module.css";
 
 const WelcomeSection = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <section className={styles.welcomeSection}>
-            <Logo />
-            <p className={styles.welcomeSubtitle}>Record daily water intake and track</p>
-            <h1 className={styles.welcomeTitle}>Water consumption tracker</h1>
-            <div className={styles.welcomeButtons}>
-                <button
-                    className={styles.tryTrackerButton}
-                    onClick={() => navigate('/signup')}
-                >
-                    Try tracker
-                </button>
-                <button
-                    className={styles.signInButton}
-                    onClick={() => navigate('/signin')}
-                >
-                    Sign In
-                </button>
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles.welcomeSection}>
+      <Logo />
+      <div className={styles.welcomeSectionWrapper}>
+        <p className={styles.welcomeSubtitle}>
+          Record daily water intake and track
+        </p>
+        <h1 className={styles.welcomeTitle}>Water consumption tracker</h1>
+        <div className={styles.welcomeButtons}>
+          <button
+            className={styles.tryTrackerButton}
+            onClick={() => navigate("/signup")}
+          >
+            Try tracker
+          </button>
+          <button
+            className={styles.signInButton}
+            onClick={() => navigate("/signin")}
+          >
+            Sign In
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default WelcomeSection;
