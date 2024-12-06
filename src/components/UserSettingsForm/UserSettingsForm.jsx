f9c48ec24733a331ca5d06175b46e5868e34f6a3
prihodko_bohdan <prihodkob15@gmail.com>
2024-12-06 08:36:54 +0200

    add translate UserSetitngsModal and Forma

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -11,12 +11,14 @@ import { selectUser, selectUserLoading } from "../../redux/user/selectors";
 
 import css from "./UserSettingsForm.module.css";
 import avatar from "../../img/default-avatar.webp";
+import { useTranslation } from "react-i18next";
 
 const UserSettingsForm = ({ setImg }) => {
   const user = useSelector(selectUser);
   const loading = useSelector(selectUserLoading);
   const [preview, setPreview] = useState(user?.data?.avatarUrl || null);
   const [recommendedDailyNorma, setRecommendedDailyNorma] = useState(null);
+  const { t } = useTranslation();
 
   const { values } = useFormikContext();
 
@@ -42,9 +44,17 @@ const UserSettingsForm = ({ setImg }) => {
     <Form className={css.formWrapper}>
       <div className={css.fileWrapper}>
         {preview ? (
-          <img className={css.avatarImg} src={preview} alt="User's avatar" />
+          <img
+            className={css.avatarImg}
+            src={preview}
+            alt={t("userSettingsForm.avatarAlt")}
+          />
         ) : (
-          <img className={css.avatarImg} src={avatar} alt="User's avatar" />
+          <img
+            className={css.avatarImg}
+            src={avatar}
+            alt={t("userSettingsForm.avatarAlt")}
+          />
         )}
         <label>
           <div className={css.iconWrapper}>
@@ -52,36 +62,46 @@ const UserSettingsForm = ({ setImg }) => {
             <Field
               type="file"
               name="avatarUrl"
-              placeholder="Upload a photo"
+              placeholder={t("userSettingsForm.uploadPhoto")}
               hidden
               onChange={handleFileChange}
             />
-            <span className={css.fileUpload}>Upload a photo</span>
+            <span className={css.fileUpload}>
+              {t("userSettingsForm.uploadPhoto")}
+            </span>
           </div>
         </label>
       </div>
 
       <div className={css.mainWrapper}>
         <div className={css.blockWrapper}>
-          <h3 className={css.inputLabel}>Your gender identity</h3>
+          <h3 className={css.inputLabel}>
+            {t("userSettingsForm.genderIdentity")}
+          </h3>
           <div className={css.checkmarkWrapper}>
             <label className={css.radioWrapper}>
               <Field type="radio" name="gender" value="female" />
               <span className={css.checkmark}></span>
-              <span>Woman</span>
+              <span>{t("userSettingsForm.genderFemale")}</span>
             </label>
             <label className={css.radioWrapper}>
               <Field type="radio" name="gender" value="male" />
               <span className={css.checkmark}></span>
-              <span>Man</span>
+              <span>{t("userSettingsForm.genderMale")}</span>
             </label>
           </div>
         </div>
         <div className={css.allInputsWrapper}>
           <div className={css.blockWrapper}>
             <label className={css.inputWrapper}>
-              <span className={css.inputLabel}>Yout name</span>
-              <Field type="text" name="name" placeholder="Your name" />
+              <span className={css.inputLabel}>
+                {t("userSettingsForm.nameLabel")}
+              </span>
+              <Field
+                type="text"
+                name="name"
+                placeholder={t("userSettingsForm.namePlaceholder")}
+              />
               <ErrorMessage
                 className={css.errorMessage}
                 name="name"
@@ -89,8 +109,14 @@ const UserSettingsForm = ({ setImg }) => {
               />
             </label>
             <label className={css.inputWrapper}>
-              <span className={css.inputLabel}>Email</span>
-              <Field type="text" name="email" placeholder="example@ua.com" />
+              <span className={css.inputLabel}>
+                {t("userSettingsForm.emailLabel")}
+              </span>
+              <Field
+                type="text"
+                name="email"
+                placeholder={t("userSettingsForm.emailPlaceholder")}
+              />
               <ErrorMessage
                 className={css.errorMessage}
                 name="email"
@@ -100,33 +126,37 @@ const UserSettingsForm = ({ setImg }) => {
           </div>
 
           <div className={css.blockWrapper}>
-            <h3 className={css.inputLabel}>My daily norma</h3>
+            <h3 className={css.inputLabel}>
+              {t("userSettingsForm.dailyNormTitle")}
+            </h3>
             <ul className={css.listWrapper}>
               <li>
-                For woman: <br />
-                <span className={css.accentText}>V=(M*0,03) + (T*0,4)</span>
+                {t("userSettingsForm.dailyNormWoman")}: <br />
+                <span className={css.accentText}>
+                  {t("userSettingsForm.formulaWoman")}
+                </span>
               </li>
               <li>
-                For man: <br />
-                <span className={css.accentText}>V=(M*0,04) + (T*0,6)</span>
+                {t("userSettingsForm.dailyNormMan")} <br />
+                <span className={css.accentText}>
+                  {t("userSettingsForm.formulaMan")}
+                </span>
               </li>
             </ul>
             <div className={css.frame}>
               <p>
-                <span className={css.accentText}>*</span> V is the volume of the
-                water norm in liters per day, M is your body weight, T is the
-                time of active sports, or another type of activity commensurate
-                in terms of loads (in the absence of these, you must set 0)
+                <span className={css.accentText}>*</span>{" "}
+                {t("userSettingsForm.formulaExplanation")}
               </p>
             </div>
             <div className={css.iconWrapper}>
               <BsExclamationLg className={css.accentIcon} />
-              <p>Active time in hours</p>
+              <p>{t("userSettingsForm.activeTimeLabel")}</p>
             </div>
           </div>
           <div className={css.blockWrapper}>
             <label className={css.inputWrapper}>
-              <span>Your weight in kilograms:</span>
+              <span>{t("userSettingsForm.weightLabel")}</span>
               <Field type="text" name="weight" placeholder="0" />
               <ErrorMessage
                 className={css.errorMessage}
@@ -135,7 +165,7 @@ const UserSettingsForm = ({ setImg }) => {
               />
             </label>
             <label className={css.inputWrapper}>
-              <span>The time of active participation in sports:</span>
+              <span>{t("userSettingsForm.sportParticipationLabel")}</span>
               <Field type="text" name="sportParticipation" placeholder="0" />
               <ErrorMessage
                 className={css.errorMessage}
@@ -147,12 +177,12 @@ const UserSettingsForm = ({ setImg }) => {
 
           <div className={css.blockWrapper}>
             <p>
-              The required amount of water in liters per day: <br />
+              {t("userSettingsForm.waterRequired")}: <br />
               <span className={css.accentText}>{recommendedDailyNorma}L</span>
             </p>
             <label className={css.inputWrapper}>
               <span className={css.inputLabel}>
-                Write down how much water you will drink:
+                {t("userSettingsForm.waterInputLabel")}
               </span>
               <Field type="text" name="dailyNorm" placeholder="1.8" />
               <ErrorMessage
@@ -165,7 +195,9 @@ const UserSettingsForm = ({ setImg }) => {
         </div>
       </div>
       <button type="submit" className={clsx("green", css.saveButton)}>
-        {loading.main ? "Loading..." : "Save"}
+        {loading.main
+          ? t("userSettingsForm.loading")
+          : t("userSettingsForm.saveButton")}
       </button>
     </Form>
   );
